06fecc819cd0482c94a1999c2539a8e0e2f6a1cf
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-01 23:22:34 -0500

    Created function to update employee role

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-
 // require all packages
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
@@ -50,6 +32,9 @@ const deptPrompt = {
     name: "deptName",
 };
 
+const init = () => {
+    openMenu()
+};
 
 const openMenu = () => {
     inquirer
@@ -62,9 +47,9 @@ const openMenu = () => {
             case 'Add Employee':
                 addEmployeeName();
                 break;
-            // case 'Update Employee Role':
-            //     updateEmp();
-            //     break;
+            case 'Update Employee Role':
+                updateEmp();
+                break;
             case 'View All Roles':
                 viewRoles();
                 break;
@@ -270,15 +255,74 @@ const addEmployee = (firstName, lastName, role_id, manager_id) => {
     })
 };
 
-openMenu();
-// Create db query for roles to show all roles, job title, role id, the department that role belongs to, and the salary for that role
+const updateEmp = () => {
+    let sql = "SELECT id, first_name, last_name, role_id FROM employee";
+    db.query(sql, (err, response) => {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Please select an employee whose role you wish to update",
+                choices: () => {
+                    const choices = [];
+                    for (let i = 0; i < response.length; i++) {
+                        let currName = `${response[i].first_name} ${response[i].last_name}`;
+                        choices.push(currName);
+                    }
+                    return choices;
+                },
+                name: "employee"
+            }
+        ]).then(answer => {
+            let id;
+            const employeeName = answer.employee.split(" ");
+            for (let i = 0; i <response.length; i++) {
+                if (response[i].first_name === employeeName[0] && response[i].last_name === employeeName[1]) {
+                    id = response[i].id;
+                }
+            };
+            askEmpRole(id);
+        })
+    })
+};
 
-// Create db query for employees to show all employees including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+const askEmpRole = (id) => {
+    let sql = `SELECT role.id AS id, role.title AS title FROM role`;
+    db.query(sql, (err, response) => {
+        if (err) throw (err);
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Please select the employee's new role: ",
+                choices: () => {
+                    const choices = [];
+                    for (let i = 0; i < response.length; i++) {
+                        choices.push(response[i].title);
+                    }
+                    return choices;
+                },
+                name: "newRole"
+            },
+        ])
+        .then(answer => {
+            let role_id;
+            for (let i = 0; i< response.length; i++) {
+                if (response[i].title === answer.newRole) {
+                    role_id = response[i].id;
+                }
+            }
+            updateEmpRole(id, role_id);            
+        })
+    })
+};
 
-// Create db query for departments to add a department
+const updateEmpRole = (id, role_id) => {
+    let sql = `UPDATE employee SET role_id = ${role_id} WHERE id = ${id}`;
+    db.query(sql, (err, response) => {
+        if (err) throw (err);
+        console.log(`Employee Role Updated`);
+        openMenu();
+    });
+}
 
-// Create db query for role to add a role
-
-// Create db query for employee to add an employee
-
-// Create db query for employee to update an employee. Should this be in the same place as to add?
+init();
