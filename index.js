cd8e62ca4ed5f7f7cd5a991df649b6348549c71c
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-01 22:54:44 -0500

    Added quit functionality to main menu

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-// Array of current employees
+// create database connection
 
 const db = mysql.createConnection(
     {
@@ -50,15 +50,6 @@ const deptPrompt = {
     name: "deptName",
 };
 
-// Create inquirer choice to select employee to update and allow their role to be updated
-const updateEmployeePrompt = [{
-    type: "list",
-    message: "Please choose an employee to update their role",
-    name: "employee",
-    // allemployees will need to use an array of the employees to generate a list of choices
-    // choices: allEmployees
-}];
-
 
 const openMenu = () => {
     inquirer
@@ -86,6 +77,8 @@ const openMenu = () => {
             case 'Add Department':
                 addDept();
                 break;
+            case 'Quit':
+                process.exit();
         }
     })
 };
@@ -175,21 +168,6 @@ const addRole = () => {
         });
     });
 };
-
-// const employeeArr = [];
-//         for (let i = 0; i<rows.length; i++) {
-//             const currentRow = Array.from(rows[i]);
-//             if(rows[i].manager_id !== null) 
-//                 {
-//                 const currManagerId = rows[i].manager_id;
-//                 const currManagerName = `${rows[currManagerId].first_name} ${rows[currManagerId].last_name}`;
-//                 currentRow.splice(6, 1, currManagerName);
-//                 employeeArr.push(currentRow);
-//                 }
-//             else {
-//                 employeeArr.push(rows[i]);
-//             }
-//         }
 
 const viewEmployees = () => {
     const sql = `SELECT a.id AS id, a.first_name AS first_name, a.last_name AS last_name, role.title AS title, department.name AS department, role.salary AS salary, CONCAT(b.first_name, ' ', b.last_name) AS manager FROM employee a LEFT JOIN employee b ON a.manager_id = b.id JOIN role ON a.role_id = role.id JOIN department ON role.dept_id = department.id `;
